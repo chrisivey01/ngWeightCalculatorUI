baf4e66f6a06c8d131535cf8ea7507dad04cc9ca
Jurbin <jurbin6090@gmail.com>
2017-09-19 09:04:14 -0500

    Moved working calc-box code to calc-box component, using test component for service tests

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {TestService} from "./test.service";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-test',
@@ -7,76 +8,25 @@ import {TestService} from "./test.service";
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  leftWeights = [];
-  rightWeights = [];
-  standardHeight = 0.45;
-  blanks = 20;
-  totalWeight = 45;
-  service;
-  fortyFives = 0;
-  twentyFives = 0;
-  tens = 0;
-  fives = 0;
-  twoPointFives = 0;
+  serviceValue;
 
-  constructor(service: TestService) {
-    this.service = service;
-
-    let weight = 290;
-    weight -= 45;
-
-    while (weight >= 5 && this.blanks > 0) {
-      if (weight >= 90) {
-        this.leftWeights.push(45);
-        this.rightWeights.push(45);
-        this.fortyFives += 2;
-        this.totalWeight += 90;
-        weight = weight - 90;
-      } else if (weight >= 50) {
-        this.leftWeights.push(25);
-        this.rightWeights.push(25);
-        this.twentyFives += 2;
-        this.totalWeight += 50;
-        weight = weight - 50;
-      } else if (weight >= 20) {
-        this.leftWeights.push(10);
-        this.rightWeights.push(10);
-        this.tens += 2;
-        this.totalWeight += 20;
-        weight = weight - 20;
-      } else if (weight >= 10) {
-        this.leftWeights.push(5);
-        this.rightWeights.push(5);
-        this.fives += 2;
-        this.totalWeight += 10;
-        weight = weight - 10;
-      } else {
-        this.leftWeights.push(2.5);
-        this.rightWeights.push(2.5);
-        this.twoPointFives += 2;
-        this.totalWeight += 5;
-        weight = weight - 5;
-      }
-      this.blanks -= 2;
-    }
-
-    this.leftWeights.reverse();
-
-    // this.testService()
+  constructor(private testService: TestService) {
+    this.test();
   }
 
   ngOnInit() {
   }
 
-  testService() {
-    this.service.testService()
+  test() {
+    this.testService.testService()
       .subscribe(
         response => {
-          console.log(response.text())
+          this.serviceValue = response.text()
         },
         err => {
           console.log(err);
         });
     ;
   }
+
 }

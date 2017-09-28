76798788d51c82f21885dbba216c67be7fdf6fbe
Jurbin <jurbin6090@gmail.com>
2017-09-28 09:32:11 -0500

    Added restricted login functionality, updated dependancies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {CalcBoxComponent} from './calc-box/calc-box.component';
 import {TitleBarComponent} from './title-bar/title-bar.component';
 import {SideBarComponent} from './side-bar/side-bar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCardModule, MdCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatCheckboxModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {Routes, RouterModule} from "@angular/router";
@@ -14,12 +14,13 @@ import {LoginComponent} from './login/login.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {CalcBoxService} from './calc-box/calc-box.service';
 import {TestService} from './test/test.service'
-import {TestComponent} from './test/test.component'
+import {LoginService} from "./login/login.service";
+import {TestComponent} from './test/test.component';
 
 const appRoutes: Routes = [
   {path: 'login-route', component: LoginComponent},
-  {path: 'calc-box-route', component: CalcBoxComponent},
-  {path: 'test-route', component: TestComponent}
+  {path: 'calc-box-route', component: CalcBoxComponent, canActivate: [LoginComponent]},
+  {path: 'test-route', component: TestComponent, canActivate: [LoginComponent]}
 ];
 
 @NgModule({
@@ -40,14 +41,14 @@ const appRoutes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MdButtonModule,
-    MdCardModule,
-    MdCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
     ReactiveFormsModule,
     HttpModule,
     FlexLayoutModule
   ],
-  providers: [CalcBoxService, TestService],
+  providers: [SideBarComponent, LoginComponent, CalcBoxService, TestService, LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
